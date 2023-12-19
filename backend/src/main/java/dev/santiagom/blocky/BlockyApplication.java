b7e4c01045fdc7cb878fe8dae3e7f9c74e6d4efe
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2023-12-18 21:26:19 -0300

    Add backend folder and initial Spring Boot app

---
+++ backend/src/main/java/dev/santiagom/blocky/BlockyApplication.java
@@ -0,0 +1,13 @@
+package dev.santiagom.blocky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/santiagom/blocky/BlockyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.santiagom.blocky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
