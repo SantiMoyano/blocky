e8646a42fb4b791330b89e66d1e39e64d51a14a9
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-02 15:06:57 -0300

    Implement register functionality with redux

--- frontend/src/components/register/Register.js
+++ frontend/src/components/register/Register.js
@@ -1,19 +1,66 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUser } from "../../redux/auth/registerSlice";
+
 function Register() {
+  const dispatch = useDispatch();
+  const registering = useSelector((state) => state.auth.registering);
+  const error = useSelector((state) => state.auth.error);
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Realizar validaciones, por ejemplo, asegurarse de que las contraseñas coincidan
+
+    // Despachar la acción para registrar al usuario
+    dispatch(registerUser(formData));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>username</label>
-        <input type="text" />
+        <label>Username</label>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <label>password</label>
-        <input type="password" />
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <label>repeat password</label>
-        <input type="password" />
+        <label>Repeat Password</label>
+        <input
+          type="password"
+          name="repeatPassword"
+          value={formData.repeatPassword}
+          onChange={handleChange}
+        />
       </div>
-      <button>submit</button>
+      <button type="submit" disabled={registering}>
+        {registering ? "Registering..." : "Submit"}
+      </button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </form>
   );
 }
