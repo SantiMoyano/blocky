95409fd0ac27079934dfc8268b12ef2364d31f13
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-02 13:33:19 -0300

    Add actions for register on redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import axios from "axios";
 import "./index.css";
 import App from "./App";
+import store from "./redux/store";
+
+axios.defaults.baseURL = "http://localhost:8080";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
