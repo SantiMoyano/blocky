d0c832b274d7a1efc07889f512db58dcea27cdfc
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-05 17:41:46 -0300

    Add Category Repository

---
+++ backend/src/main/java/dev/santiagom/blocky/tables/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package dev.santiagom.blocky.tables.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
