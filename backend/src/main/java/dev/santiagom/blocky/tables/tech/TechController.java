bd01e18f5761a0e4983c4bc4da74e5b94ae4d08b
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-05 16:20:50 -0300

    Implement functionality to get all tech

--- backend/src/main/java/dev/santiagom/blocky/tables/tech/TechController.java
+++ backend/src/main/java/dev/santiagom/blocky/tables/tech/TechController.java
@@ -1,11 +1,25 @@
 package dev.santiagom.blocky.tables.tech;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/tech")
 @CrossOrigin(origins = {"http://localhost:3000"})
 public class TechController {
+
+    @Autowired
+    private TechService techService;
+
+    @GetMapping
+    public ResponseEntity<List<Tech>> getAllTech() {
+        return new ResponseEntity<List<Tech>>(techService.allTech(), HttpStatus.OK);
+    }
 }
--- backend/src/main/java/dev/santiagom/blocky/tables/tech/TechService.java
+++ backend/src/main/java/dev/santiagom/blocky/tables/tech/TechService.java
@@ -1,11 +1,19 @@
 package dev.santiagom.blocky.tables.tech;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TechService {
 
     @Autowired
     private TechRepository techRepository;
+
+    public List<Tech> allTech() {
+        return techRepository.findAll();
+    }
 }
