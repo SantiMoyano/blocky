1729fdec17362091afea63c48e2b5b883234fc55
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-05 17:36:06 -0300

    Implement functionality to get all tasks

--- backend/src/main/java/dev/santiagom/blocky/tables/task/TaskController.java
+++ backend/src/main/java/dev/santiagom/blocky/tables/task/TaskController.java
@@ -1,11 +1,25 @@
 package dev.santiagom.blocky.tables.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/task")
 @CrossOrigin(origins = {"http://localhost:3000"})
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTask() {
+        return new ResponseEntity<List<Task>>(taskService.allTask(), HttpStatus.OK);
+    }
 }
