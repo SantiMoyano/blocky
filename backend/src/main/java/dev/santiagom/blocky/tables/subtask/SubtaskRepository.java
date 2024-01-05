08066e1a08615c00c55ab7572588ee61f2fe5d2e
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-05 18:18:04 -0300

    Add subtask Repository

---
+++ backend/src/main/java/dev/santiagom/blocky/tables/subtask/SubtaskRepository.java
@@ -0,0 +1,6 @@
+package dev.santiagom.blocky.tables.subtask;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubtaskRepository extends JpaRepository<Subtask, Long> {
+}
