8405d10202cde815ecc31d6f176252f1cb64d102
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-05 18:19:09 -0300

    Add subtask Controller

---
+++ backend/src/main/java/dev/santiagom/blocky/tables/subtask/SubtaskController.java
@@ -0,0 +1,11 @@
+package dev.santiagom.blocky.tables.subtask;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/subtask")
+@CrossOrigin(origins = {"http://localhost:3000"})
+public class SubtaskController {
+}
