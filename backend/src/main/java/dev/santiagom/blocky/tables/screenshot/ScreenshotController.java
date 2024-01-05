c5ad5497c85afdbc4a0f3d9eae4edc2323159224
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-05 12:25:17 -0300

    Add screenshot controller

---
+++ backend/src/main/java/dev/santiagom/blocky/tables/screenshot/ScreenshotController.java
@@ -0,0 +1,12 @@
+package dev.santiagom.blocky.tables.screenshot;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/screenshot")
+@CrossOrigin(origins = {"http://localhost:3000"})
+public class ScreenshotController {
+    
+}
