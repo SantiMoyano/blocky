129aea46209562be4eef4475bf856523329f7ea4
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-05 17:12:58 -0300

    Implement functionality to get all epics

--- backend/src/main/java/dev/santiagom/blocky/tables/epic/EpicController.java
+++ backend/src/main/java/dev/santiagom/blocky/tables/epic/EpicController.java
@@ -1,11 +1,25 @@
 package dev.santiagom.blocky.tables.epic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/epic")
 @CrossOrigin(origins = {"http://localhost:3000"})
 public class EpicController {
+
+    @Autowired
+    private EpicService epicService;
+
+    @GetMapping
+    public ResponseEntity<List<Epic>> getAllEpics() {
+        return new ResponseEntity<List<Epic>>(epicService.allEpics(), HttpStatus.OK);
+    }
 }
