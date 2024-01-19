0abdd09cb985e65292cd670948fcec0f75027cc5
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-19 12:10:10 -0300

    Add CreateEpic Component

---
+++ frontend/src/components/epics/CreateEpic.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createEpic } from "../../redux/epics/createEpicSlice";
+import Form from "../form/Form";
+import Notification from "../notification/Notification";
+
+function CreateEpic({ loadEpics, projectId }) {
+  const dispatch = useDispatch();
+  const { creating, success, error } = useSelector((state) => state.createEpic);
+
+  const [epicRequest, setEpicRequest] = useState({
+    name: "",
+    description: "",
+    projectId: projectId,
+  });
+
+  const epicData = [
+    {
+      label: "Epic Name",
+      type: "text",
+      name: "name",
+      value: epicRequest.name,
+    },
+    {
+      label: "Description",
+      type: "text",
+      name: "description",
+      value: epicRequest.description,
+    },
+  ];
+
+  function handleChange(e) {
+    setEpicRequest({
+      ...epicRequest,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(createEpic(epicRequest));
+    setTimeout(() => loadEpics(), 50);
+  }
+
+  return (
+    <div>
+      <h1>CREATE EL EPIC</h1>
+      <Form
+        formData={epicData}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        buttonInfo="Create epic"
+      />
+
+      {success && (
+        <Notification message="Epic created successfully" type="success" />
+      )}
+      {error && <Notification message="An error has occurred" type="error" />}
+    </div>
+  );
+}
+
+export default CreateEpic;
--- frontend/src/components/epics/Epics.js
+++ frontend/src/components/epics/Epics.js
@@ -1,26 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllEpics } from "../../redux/epics/epicsSlice";
 import { useNavigate } from "react-router-dom";
 import BlockSection from "../blocks/BlockSection";
+import CreateEpic from "./CreateEpic";
 
 function Epics({ projectId }) {
   const dispatch = useDispatch();
+  const [showForm, setShowForm] = useState(false);
   const { epics, loading, error } = useSelector((state) => state.epics);
   const navigate = useNavigate();
 
   useEffect(() => {
     // Dispatch the action to get all epics for the specified project
-    dispatch(getAllEpics(projectId));
-  }, [dispatch, projectId]);
+    loadEpics(projectId);
+  });
 
   function handleEpicClick(epicId) {
     navigate(`/epic/${epicId}`);
   }
 
+  function loadEpics(projectId) {
+    dispatch(getAllEpics(projectId));
+  }
+
   return (
     <>
       {/* Add any filtering or other options here if needed */}
+      <button onClick={() => setShowForm(!showForm)}>Add project +</button>
+      {showForm && <CreateEpic loadEpics={loadEpics} />}
       <BlockSection list={epics} handleElemClick={handleEpicClick} />
     </>
   );
