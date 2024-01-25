18ed693ecfdbabbc61b9053066004a76eb20276c
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2024-01-24 22:27:21 -0300

    Add UpdateEpic Component

--- frontend/src/components/epics/DetailedEpic.js
+++ frontend/src/components/epics/DetailedEpic.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getEpicDetails } from "../../redux/epics/epicDetailSlice";
 import { useParams } from "react-router-dom";
@@ -7,6 +7,7 @@ import Tasks from "../tasks/Tasks";
 function DetailedEpic() {
   const dispatch = useDispatch();
   const { epicId } = useParams();
+  const [showEditForm, setShowEditForm] = useState();
   const { epic, loading, error } = useSelector((state) => state.epic);
 
   useEffect(() => {
@@ -19,10 +20,20 @@ function DetailedEpic() {
 
   if (error) return <p>Error: {error}</p>;
 
+  function handleEdit() {
+    setShowEditForm(!showEditForm);
+  }
+
+  function loadEpic() {
+    console.log("reload");
+  }
+
   return (
     <>
       <section>
         <h2>Detailed Epic {epicId}</h2>
+        <button onClick={handleEdit}>Edit task</button>
+        {showEditForm && <UpdateEpic epic={epic} loadEpic={loadEpic} />}
         <div className="epic-info">
           <p>{epic.name}</p>
           <p>{epic.progress}</p>
---
+++ frontend/src/components/epics/UpdateEpic.js
@@ -0,0 +1,68 @@
+import { useDispatch, useSelector } from "react-redux";
+import Form from "../form/Form";
+import Notification from "../notification/Notification";
+import { useState } from "react";
+
+function UpdateEpic({ epic, loadEpic }) {
+  const dispatch = useDispatch();
+  const { updating, success, error } = useSelector((state) => state.updateEpic);
+
+  const [epicRequest, setEpicRequest] = useState({
+    name: epic.name,
+    description: epic.description,
+    projectId: epic.projectId,
+  });
+
+  const epicData = [
+    {
+      label: "Epic Name",
+      type: "text",
+      name: "name",
+      value: epicRequest.name,
+    },
+    {
+      label: "Description",
+      type: "text",
+      name: "description",
+      value: epicRequest.description,
+    },
+  ];
+
+  function handleChange(e) {
+    setEpicRequest({
+      ...epicRequest,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(
+      updateEpic({
+        projectId: epic.projectId,
+        request: epicRequest,
+      })
+    );
+    setTimeout(() => {
+      loadEpic();
+    }, 50);
+  }
+
+  return (
+    <div>
+      <h1>Update Epic</h1>
+      <Form
+        formData={epicData}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        buttonInfo="Update epic"
+      />
+      {success && (
+        <Notification message="Epic updated successfully" type="success" />
+      )}
+      {error && <Notification message="An error has occurred" type="error" />}
+    </div>
+  );
+}
+
+export default UpdateEpic;
